f20c45234e3c02620b4bd83cbef1445c4a8d9193
John Paz <john.paz@menlosecurity.com>
2021-08-09 15:53:12 -0400

    Updated scripts to use Paligo output filename, instead of 'archive.zip'.

--- scripts/delete.py
+++ scripts/delete.py
@@ -5,7 +5,7 @@ try:
     print('Deleted unneeded sub-directories...')
     os.rmdir('../7255-Documentation_Hub-html5/out')
     os.rmdir('../7255-Documentation_Hub-html5')
-    os.remove('../archive.zip')
+    os.remove('../7255-Documentation_Hub-html5_-Upload_to_GitHub.zip')
     
 except:
    print('Unable to finish deleting...')
