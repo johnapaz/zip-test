aa4064044f0ca38c6efdef8f41f3fe6bfa394aee
John Paz <john.paz@menlosecurity.com>
2021-08-10 12:26:13 -0400

    Fixing broken build.

--- scripts/delete.py
+++ scripts/delete.py
@@ -3,15 +3,9 @@ import os, glob
 
 try:
     print('Deleted unneeded sub-directories...')
-<<<<<<< HEAD
-    os.rmdir('../7255-Documentation_Hub-html5/out')
-    os.rmdir('../7255-Documentation_Hub-html5')
-    os.remove('../7255-Documentation_Hub-html5_-Upload_to_GitHub.zip')
-=======
     os.rmdir('./7255-Documentation_Hub-html5/out')
     os.rmdir('./7255-Documentation_Hub-html5')
-    os.remove('./archive.zip')
->>>>>>> d0add90111130cd606363e03e84d0f981bc2cd9c
+    os.remove('./7255-Documentation_Hub-html5_-Upload_to_GitHub.zip')
     
 except:
    print('Unable to finish deleting...')
